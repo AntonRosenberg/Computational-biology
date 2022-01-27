f0e8000b3e3df1a0015bf8b5e7a8a574bcea17ae
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-01-27 16:41:44 +0100

    Fix 3a and some 3b

--- First homework/Exercise 3/main.py
+++ First homework/Exercise 3/main.py
@@ -1,4 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+    alpha = 0.01
+    numGen = 300
+    R_list = [1+0.1*_ for _ in range(291)]
+    eta = np.zeros([numGen, len(R_list)])
+    print(np.shape(eta))
+    plt.subplot(1,2,1)
+    for j, R in enumerate(R_list):
+        eta[0, j] = 900
+        for i in range(numGen-1):
+            eta[i+1, j] = np.copy(R*eta[i, j]*np.exp(-alpha*eta[i, j]))
 
+    for i in range(100):
+        plt.plot(R_list, eta[-100+i, :])
 
+    plt.ylabel('\u03B7')
+    plt.xlabel('R')
+
+    plt.subplot(1, 2, 2)
+    indexRs = [0]
+    for l in indexRs:
+        plt.plot(np.linspace(1,numGen,numGen),eta[:,l])
+
+    plt.show()
