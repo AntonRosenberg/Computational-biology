d65687b0d913000f8d6c514f854e724036fd20e0
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-02-10 12:53:32 +0100

    fixes

--- First homework/Exercise1/main.py
+++ First homework/Exercise1/main.py
@@ -15,6 +15,13 @@ def detect_oscillation(X):
     dx = np.diff(X)
     return not np.all(dx >= -1e-3)
 
+def detect_limitcycle(X):
+    X = round(X,3)
+    max1=X[argrelextrema(X, np.greater)[0]]
+    max2=X[argrelextrema(X, np.greater)[1]]
+    if round(max1/max2,2)==1:
+        return True
+    return False
 
 if __name__ == '__main__':
     global r, A, K, N0
@@ -23,7 +30,8 @@ if __name__ == '__main__':
     K=100
     N0=50
 
-    T = [0.1*_ for _ in range(38, 41)]
+    T = [0.1*_ for _ in range(30, 45)]
+
     tmax = 800
     ts = np.linspace(0, tmax, 100*tmax)
     Ns = []
@@ -46,14 +54,19 @@ if __name__ == '__main__':
     plt.xlabel('t')
     plt.ylabel('N(t)')
     plt.legend(loc="lower right")
+
+    for i,sol in enumerate(Ns):
+        if detect_limitcycle(Ns[i])==True:
+            Tcritical = T[i]
+            break
     '''
     for i,sol in enumerate(Ns):
         if detect_oscillation(Ns[i])==True:
             Tcritical = T[i]
             break
-    
-    print(f'Tcritical = {Tcritical}')
     '''
+    print(f'Tcritical = {Tcritical}')
+
     plt.subplot(1, 2, 2)
     Ndot = np.zeros([len(Ns), len(Ns[0])])
     for i, sol in enumerate(Ns):
@@ -61,7 +74,7 @@ if __name__ == '__main__':
             if t-100*delay>0:
                 Ndot[i,t] = r * sol[t] * (1 - sol[round(t-100*T[i])]/K)*(sol[t]/A-1)
             else:
-                Ndot[i,t]= r * sol[t] * (1 - N0 / K) * (sol[t] / A - 1)
+                Ndot[i,t] = r * sol[t] * (1 - N0 / K) * (sol[t] / A - 1)
         '''
         Nrounded = [round(value,5) for value in Ndot[i,:]]
         if len(set(Nrounded)) != len(Nrounded):
